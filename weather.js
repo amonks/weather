29bfe10ababd6af5398c7b4ca95f0ac357a498a4
Andrew Monks <a@monks.co>
2014-03-26 15:04:17 -0500

    cloud

--- weather.js
+++ weather.js
@@ -65,7 +65,7 @@ Weather = {
 
             for (var xI = canvasX - 1; xI >= 0; xI--) {
                 if (xI % Weather.grid === 0) {
-                    for (var yI = canvasX - 1; yI >= 0; yI--) {
+                    for (var yI = canvasY - 1; yI >= 0; yI--) {
                         if (yI % Weather.grid === 0) {
                             if (Math.random() <= cloudCover) {
                                 context.drawImage(imageObj, xI, yI);
