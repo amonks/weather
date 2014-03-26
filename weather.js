a1180d81cb2e7663488e00a7af80d09156d2ff6c
Andrew Monks <a@monks.co>
2014-03-26 11:15:46 -0500

    index

--- weather.js
+++ weather.js
@@ -1,6 +1,10 @@
 var forecastIOKey = "0ea12a6cd58c887c14ebd67679e11701";
 x = $('#demo');
 
+function showCloudCover(weather) {
+    x.innerHTML = weather.currently.cloudCover;
+}
+
 function getForecast(forecastIOKey, position) {
 	url = "https://api.forecast.io/forecast/" + forecastIOKey + "/" + position.latitude + "," + position.longitude;
     out = Bouncer.get(url);
@@ -10,14 +14,17 @@ function getForecast(forecastIOKey, position) {
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
+        navigator.geolocation.getCurrentPosition(getWeather, showError);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
-function showPosition(position) {
-    x.text( getForecast(forecastIOKey, position.coords).response );
+function getWeather(position) {
+    out = getForecast(forecastIOKey, position.coords).response;
+    weather = JSON.parse(out);
+
+    showCloudCover(weather);
 }
 
 function showError(error) {
