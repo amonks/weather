5ff513f232c2c315e9c01b0430fc60d183af4905
Andrew Monks <a@monks.co>
2014-03-26 14:25:06 -0500

    index

--- weather.js
+++ weather.js
@@ -1,46 +1,50 @@
-var forecastIOKey = "0ea12a6cd58c887c14ebd67679e11701";
-x = $('#demo');
+Weather = {
 
-function showCloudCover(cloudCover) {
-    x.text( cloudCover );
-}
+    isLoaded: false,
 
-function getForecast(forecastIOKey, position) {
-	url = "https://api.forecast.io/forecast/" + forecastIOKey + "/" + position.latitude + "," + position.longitude;
-    out = Bouncer.get(url);
-    console.log(url);
-    return out;
-}
+    forecastIOKey: "0ea12a6cd58c887c14ebd67679e11701",
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getWeather, showError);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+    showCloudCover: function(cloudCover) {
+        x.text( cloudCover );
+    },
+
+    getForecast: function(position) {
+    	url = "https://api.forecast.io/forecast/" + Weather.forecastIOKey + "/" + position.latitude + "," + position.longitude;
+        console.log("fetching weather from " + url);
+        out = Bouncer.get(url);
+        Weather.isLoaded = true;
+        return out;
+    },
+
+    getLocation: function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(doWeather, Weather.showError);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
     }
-}
 
-function getWeather(position) {
-    out = getForecast(forecastIOKey, position.coords).response;
-    weather = JSON.parse(out);
-    cloudCover = weather.currently.cloudCover;
-    console.log(cloudCover);
-    showCloudCover(cloudCover);
-}
+    doWeather: function(position) {
+        out = Weather.getForecast(position.coords).response;
+        weather = JSON.parse(out);
+        var cloudCover = weather.currently.cloudCover;
+        console.log(cloudCover);
+        Weather.showCloudCover(cloudCover);
+    },
 
-function showError(error) {
-    switch (error.code) {
-        case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable."
-            break;
-        case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out."
-            break;
-        case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred."
-            break;
-    }
-}
+    showError: function(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                x.innerHTML = "User denied the request for Geolocation."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                x.innerHTML = "Location information is unavailable."
+                break;
+            case error.TIMEOUT:
+                x.innerHTML = "The request to get user location timed out."
+                break;
+            case error.UNKNOWN_ERROR:
+                x.innerHTML = "An unknown error occurred."
+                break;
+        }
+    },
